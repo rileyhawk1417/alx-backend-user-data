82b303d690e811951a0991b3aa01be1cb7d1aefd
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-13 15:56:54 +0200

    feat: add in auth class for authentication

---
+++ 0x01-basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Module for authentication
+"""
+from typing import List, TypeVar
+from flask import Flask
+
+
+class Auth:
+    """
+    Class to handle everything related to Auth
+    """
+
+    def require_auth(
+        self,
+        path: str,
+        excluded_paths: List[str],
+    ) -> bool:
+        """
+        Check if auth path(url) is valid
+        """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        # Handle slash
+        if path[-1] == '/':
+            path = path[:-1]
+        has_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                has_slash = True
+
+            if excluded_path.endswith('*'):
+                pos_after_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[pos_after_slash:-1]
+
+                pos_after_slash = path.rfind('/') + 1
+                tmp_path = path[pos_after_slash:]
+                if excluded in tmp_path:
+                    return False
+            if has_slash:
+                has_slash = False
+        path += '/'
+
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(
+        self,
+        request=None
+    ) -> str:
+        """Return Auth Header if available"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(
+        self,
+        request=None
+    ) -> TypeVar('User'):
+        """
+        Fetch the current user
+        """
+        return None
