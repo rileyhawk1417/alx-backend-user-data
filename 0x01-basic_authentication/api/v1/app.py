5417ccb7af5ecdcf7c53474c194482291c43c76e
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-13 15:05:06 +0200

    feat: add forbidden handler

--- 0x01-basic_authentication/api/v1/app.py
+++ 0x01-basic_authentication/api/v1/app.py
@@ -22,10 +22,17 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def not_found(error) -> str:
+def unauthorized_handler(error) -> str:
     """ Unauthorized request handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_handler(error) -> str:
+    """ Forbidden request handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
