5944282f4b3afbd216e34ad4e79ea16973fc2155
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-17 20:50:00 +0200

    fix: move around if statements

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -26,22 +26,21 @@ def filter_request():
     """
     Filters a request when it comes througvh
     """
-    if auth is None:
-        return
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/',
-    ]
-    if auth.require_auth(request.path, excluded_paths) is None:
-        user = auth.current_user(request)
-        if auth.authorization_header(request) is None \
-                and auth.session_cookie(request) is None:
-            abort(401)
-        if user is None:
-            abort(403)
-        request.current_user = user
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None \
+                    and auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 @app.errorhandler(404)
