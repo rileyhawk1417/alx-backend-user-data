376818ce50af88a7a20431dd189058d326d8b457
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-18 10:39:08 +0200

    fix: add env value for auth type

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-if getenv('AUTH_TYPE') == 'auth':
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif getenv('AUTH_TYPE') == 'basic_auth':
+if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
