c57d124c1dac2520f6919da719892d4db54d5532
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-21 16:56:26 +0200

    fix: remove typing for init

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -99,7 +99,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
     SEPARATOR = ';'
 
-    def __init__(self, fields: typing.List[str]) -> None:
+    def __init__(self, fields) -> None:
         """
         Init method for class
         """
