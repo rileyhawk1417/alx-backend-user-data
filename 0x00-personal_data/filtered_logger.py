90233193d5c544474c478e68f23d23ddc0479d56
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-25 11:18:09 +0200

    fix: remove other parts of the code for task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,8 @@
 Module to filter logs
 """
 
-import os
 import re
 import logging
-import mysql.connector
 from typing import List
 
 patterns = {
@@ -34,62 +32,6 @@ def filter_datum(
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
-def get_logger() -> logging.Logger:
-    """
-    Create new logger for user data
-    Returns:
-        logging.Logger: Log record
-    """
-    logger = logging.getLogger('user_data')
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    logger.addHandler(stream_handler)
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Create a mysql connection to database
-    """
-    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME', '')
-    db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    db_pass = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pass,
-        database=db_name
-    )
-    return connection
-
-
-def entryPoint():
-    """
-    Log information about user
-    """
-    fields = 'name,email,phone,ssn,password,ip,last_login,user_agent'
-    columns = fields.split(',')
-    query = 'SELECT {} FROM users'.format(fields)
-    logger = get_logger()
-    conn = get_db()
-    with conn.cursor() as cursor:
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        for row in rows:
-            record = map(
-                lambda x: '{}={}'.format(x[0], x[1]),
-                zip(columns, row)
-            )
-            msg = '{};'.format('; '.join(list(record)))
-            args = ('user_data', logging.INFO, None, None, msg, None, None)
-            log_entry = logging.LogRecord(*args)
-            logger.handle(log_entry)
-
-
 class RedactingFormatter(logging.Formatter):
     """
     Redacting Formatter
@@ -111,7 +53,3 @@ class RedactingFormatter(logging.Formatter):
         msg = super(RedactingFormatter, self).format(record)
         output = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
         return output
-
-
-if __name__ == '__main__':
-    entryPoint()
